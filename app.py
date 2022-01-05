b90beb0f406b073d8fb8147a9998cc5d3e8d92b6
Alex Tessaract <87167056+AlexTessaract@users.noreply.github.com>
2022-01-05 12:06:36 +0300

    Update app.py
    init

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, url_for
 from flask_bootstrap import Bootstrap
 from camera import VideoCamera
 
