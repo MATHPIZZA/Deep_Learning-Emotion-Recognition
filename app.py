8bd29fd6e46d0815849212b54228e12d7edbb0e8
Alex Tessaract <87167056+AlexTessaract@users.noreply.github.com>
2022-02-01 21:04:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port="5000")
+    app.run(host='0.0.0.0', threaded=True)
 
 
