f0d4e0464c270b59ca99f84f61096014a0fed976
Alex Tessaract <87167056+AlexTessaract@users.noreply.github.com>
2022-02-08 20:27:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ def gen(video):
         frame = video.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        return Response(gen(VideoCamera()),
+        return Response(gen(Video()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()),
+    return Response(gen(Video()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
