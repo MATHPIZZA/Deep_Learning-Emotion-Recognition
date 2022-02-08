7ce8ab67dd9a9a14074c557c1b0baeda9d3f943c
Alex Tessaract <87167056+AlexTessaract@users.noreply.github.com>
2022-02-08 11:25:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
+#@app.route('/video_feed')
+def video():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
